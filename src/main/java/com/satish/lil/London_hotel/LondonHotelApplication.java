72227e991f7683d0a73bdbbe61ae9e3f805f885b
sathish-chanda <sathishchandas@gmail.com>
2025-10-29 03:41:52 -0400

    Initial set up of the project which is created from starter.spring.io and then added the spring-boot-starter-web, h2, and lombok dependency.

---
+++ src/main/java/com/satish/lil/London_hotel/LondonHotelApplication.java
@@ -0,0 +1,13 @@
+package com.satish.lil.London_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LondonHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LondonHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satish/lil/London_hotel/LondonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.lil.London_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
